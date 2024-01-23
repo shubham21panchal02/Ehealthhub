981a498cbfa9a4d974aef4e93ca9ff2e4ed3d671
dhruvdkp <dkpanchal7070@gmail.com>
2024-01-23 12:19:06 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@
 import 'package:devloperproject1/Login.dart';
 import 'package:devloperproject1/Register.dart';
 import 'package:devloperproject1/User/Firstpage.dart';
+import 'package:devloperproject1/User/Secondpage.dart';
 import 'package:flutter/material.dart';
 import 'Splash.dart';
 
 void main() {
   runApp(Material(child:MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Ufirstpage(),
+    home: USecondpage(),
   )));
 }
